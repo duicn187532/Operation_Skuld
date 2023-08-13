ab95e5a2e2ec65ddde260b848603d072df16bd82
DollM <duicn187532@gmail.com>
2023-08-13 11:27:34 +0800

    new file新黨

---
+++ exercise19.py
@@ -0,0 +1,19 @@
+import urllib.request as req
+url="http://wwwk.eyny.com/index.php"
+
+request=req.Request(url,headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+})
+with req.urlopen(request) as response:
+    data=response.read().decode("utf-8")
+
+import bs4
+root=bs4.BeautifulSoup(data,"html.parser")
+titles=root.find_all("li",class_="li_orange")
+#print(titles.a.string)
+with open("data.txt","w",encoding="utf-8")as file:
+    for title in titles:
+        if title.a !=None:
+            print(title.a.string)
+            file.write(title.a.string+"\n")
+    
